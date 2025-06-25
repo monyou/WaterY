e693f567bc69d138bc07c4ff0de0107471b50d0d
Simeon Mechkov <montishak@gmail.com>
2025-06-25 15:08:46 +0300

    Working

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+declare module 'virtual:pwa-register' {
+    export type RegisterSWOptions = {
+        immediate?: boolean;
+        onNeedRefresh?: () => void;
+        onOfflineReady?: () => void;
+        onRegistered?: (swScriptUrl: string) => void;
+        onRegisterError?: (error: any) => void;
+    };
+
+    export function registerSW(options?: RegisterSWOptions): {
+        updateServiceWorker: (reloadPage?: boolean) => void;
+    };
+}
