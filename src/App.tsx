6fed40eb60be0e934a6381c009c35fbaed940098
Simeon Mechkov <montishak@gmail.com>
2025-06-25 15:31:27 +0300

    Working

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import { createSignal, onCleanup, onMount, Show } from "solid-js";
 import GlobalLoader from "./components/Loader";
 import Toast from "./components/Toast";
 import Routing from "./pages/Routing";
-import useSWUpdate from "./utils/useSWUpdate";
 
 const App = () => {
     const [showUpdate, setShowUpdate] = createSignal(false);
-    const { updateServiceWorker } = useSWUpdate();
 
     onMount(() => {
         const handler = () => setShowUpdate(true);
@@ -24,13 +22,11 @@ const App = () => {
             <Toast />
 
             <Show when={showUpdate()}>
-                <div class="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-600 text-white p-3 rounded shadow">
+                <div class="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-secondary w-[280px] text-white p-3 rounded shadow">
                     New version available.
                     <button
-                        class="ml-4 bg-white text-blue-600 px-3 py-1 rounded"
-                        onClick={() => {
-                            updateServiceWorker(true);
-                        }}
+                        class="ml-4 bg-white text-primary px-3 py-1 rounded cursor-pointer hover:bg-primary hover:text-white transition-colors"
+                        onClick={() => window.location.reload()}
                     >
                         Reload
                     </button>
--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,22 @@ render(App, root!);
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/service-worker.js")
-        .then(() => {
+        .then((registration) => {
             console.log("[App] Custom SW registered ;)");
+            registration.onupdatefound = () => {
+                const newWorker = registration.installing;
+                if (newWorker) {
+                    newWorker.onstatechange = () => {
+                        if (
+                            newWorker.state === "installed" &&
+                            navigator.serviceWorker.controller
+                        ) {
+                            // A new version is available
+                            window.dispatchEvent(new Event("sw-update"));
+                        }
+                    };
+                }
+            };
         })
         .catch((err) => {
             console.error("[App] SW registration failed:", err);
